f7df5f68f0fb6e03aca79c0d75ddad2cb11d99be
felixschmitz <felix.schmitz@gmail.com>
2017-06-15 17:06:41 +1000

    NONE: Use Fabric8 maven plugin for deployment into K8.

--- src/main/java/com/darumatic/K8ImageSecretController.java
+++ src/main/java/com/darumatic/K8ImageSecretController.java
@@ -1,6 +1,5 @@
 package com.darumatic;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,10 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author felixschmitz
  * @since 1.0
  */
-@Controller
-public class K8SecretController
+public class K8ImageSecretController
 {
-    private static final String SECRET_HTML_TEMPLATE = "secret";
+    private static final String SECRET_HTML_TEMPLATE = "imagesecret";
 
     @RequestMapping("/imagesecret")
     public String secret(Model model) {
