c91e5e55fdb1fde4cecf80a547d64d92686452e6
felixschmitz <felix.schmitz@gmail.com>
2017-06-16 12:49:23 +1000

    NONE: Updated deployment to include secret mount and to deploy service as type NodePort.

--- src/main/java/com/darumatic/K8ImageSecretController.java
+++ src/main/java/com/darumatic/K8ImageSecretController.java
@@ -1,5 +1,6 @@
 package com.darumatic;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @author felixschmitz
  * @since 1.0
  */
+@Controller
 public class K8ImageSecretController
 {
     private static final String SECRET_HTML_TEMPLATE = "imagesecret";
--- src/main/java/com/darumatic/K8SecretController.java
+++ src/main/java/com/darumatic/K8SecretController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.File;
+
 /**
  * @author felixschmitz
  * @since 1.0
@@ -13,9 +15,15 @@ public class K8SecretController
 {
     private static final String SECRET_HTML_TEMPLATE = "secret";
 
-    @RequestMapping("/imagesecret")
+    @RequestMapping("/secret")
     public String secret(Model model) {
 
+        File secretDir = new File("/etc/foo");
+        String[] list = secretDir.list();
+        for (String file : list)
+        {
+            System.out.println("Found file '" + file + "'");
+        }
         return SECRET_HTML_TEMPLATE;
     }
 }
